9bea0e398af90c6f676d0909bb5546807f07d898
Emmanuel Adebayo <eadebayo15@gmail.com>
2025-12-30 16:35:57 +0100

    feat: add ComputerModel, ComputerModelContainer, ConsoleModel, ConsoleModelContainer, MugModel, and MugModelContainer components

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+export const services = [
+  {
+    id: 1,
+    img: '/service1.png',
+    title: 'Web Development',
+    counter: 35,
+  },
+  {
+    id: 2,
+    img: '/service2.png',
+    title: 'Product Design',
+    counter: 23,
+  },
+  {
+    id: 3,
+    img: '/service3.png',
+    title: 'Branding',
+    counter: 46,
+  },
+];
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,34 @@
+import { services } from './../index';
+
 import './Services.scss';
 
 const Services = () => {
   return (
-    <div className='services'>Services</div>
+    <div className='services'>
+      <div className='services__section left'>
+        <h1 className='services__title'>How do I help?</h1>
+
+        <div className='services__list'>
+          {services.map((service) => {
+            const { id, img, title, counter } = service;
+            return (
+              <div key={id} className='services__list--item'>
+                <div className='services__list--icon'>
+                  <img src={img} alt={title} />
+                </div>
+
+                <div className='services__list--info'>
+                  <h2>{title}</h2>
+                  <h3>{counter}</h3>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className='services__section right'>Right</div>
+    </div>
   );
 };
 
